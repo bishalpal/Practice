0f5d4ad2a72ef4210eeaaf2bd3861ef9e4dd8490
bishalpal <bishaldg47@gmail.com>
2022-04-27 20:21:38 +0530

    n raised to power p

--- src/recursion/NRaiseToPowerP.java
+++ src/recursion/NRaiseToPowerP.java
@@ -1,9 +1,22 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class NRaiseToPowerP {
 
+	public static int power(int n, int p) {
+		if(p == 0) {
+			return 1;
+		}
+		return (int) (n * Math.pow(n, p-1));
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int p = sc.nextInt();
+		sc.close();
+		System.out.println(n + " raised to the power " + p + " is " + power(n,p));
 
 	}
 
