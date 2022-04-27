3fef67d45c218303f9cbada0754e5eea5cb1e6ac
bishalpal <bishaldg47@gmail.com>
2022-04-27 20:25:57 +0530

    factorial of a number

---
+++ src/recursion/FactorialOfN.java
@@ -0,0 +1,20 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class FactorialOfN {
+	
+	public static int factorial(int n) {
+		if(n == 0)
+			return 1;
+		return n*factorial(n-1);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		System.out.println(n + "! = " + factorial(n));
+		
+	}
+
+}
